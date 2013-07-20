9db6b9850b4c579dc12a8535b00b12833450e1a5
maikolaraya <maraya@lacreativeria.com>
2013-07-20 11:16:57 -0600

    Initial Commit

---
+++ testAFNTWKNG/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testAFNTWKNG
+//
+//  Created by Maikol Araya on 7/20/13.
+//  Copyright (c) 2013 La Creativería. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testAFNTWKNG/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testAFNTWKNG
+//
+//  Created by Maikol Araya on 7/20/13.
+//  Copyright (c) 2013 La Creativería. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
